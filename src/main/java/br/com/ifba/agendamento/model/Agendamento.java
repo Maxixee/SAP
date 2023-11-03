4da3ae0a8bc4cee97f2669f52861cadabc45058f
Mendes-Matheus <msmmendes211@gmail.com>
2023-11-03 08:53:24 -0300

    desfazendo o erro da classe Agendamento

--- src/main/java/br/com/ifba/agendamento/model/Agendamento.java
+++ src/main/java/br/com/ifba/agendamento/model/Agendamento.java
@@ -5,31 +5,20 @@
 package br.com.ifba.agendamento.model;
 
 import br.com.ifba.infrastructure.model.PersistenceEntity;
-import br.com.ifba.paciente.model.Paciente;
-import java.io.Serializable;
 import java.util.Calendar;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
 import lombok.Data;
 
 /**
  *
- * @author msmmendes
+ * @author davia
  */
 @Entity
 @Table(name="agendamento")
 @Data
-public class Agendamento extends PersistenceEntity implements Serializable{
-    
-    @OneToOne(mappedBy = "agendamento")
-    Paciente paciente;
-    
-    @Temporal(javax.persistence.TemporalType.DATE)
+public class Agendamento extends PersistenceEntity{
     private Calendar dataAgendamento;
-    
-    @Temporal(javax.persistence.TemporalType.TIME)
-    private Calendar horaAgendamento;
-    
+    //private Calendar horaAgendamento;
+
 }
