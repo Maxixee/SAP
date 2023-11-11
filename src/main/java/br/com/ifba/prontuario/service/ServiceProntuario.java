8724139311eb2794e0eaf23afa2869c3ac246eb3
Lara Rodrigues <lararods73@gmail.com>
2023-11-11 09:31:52 -0300

    melhoras ao ServiceProntuario

--- src/main/java/br/com/ifba/prontuario/service/ServiceProntuario.java
+++ src/main/java/br/com/ifba/prontuario/service/ServiceProntuario.java
@@ -9,53 +9,103 @@ import br.com.ifba.prontuario.model.Prontuario;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import br.com.ifba.infrastructure.exception.BusinessException;
 
 /**
  *
  * @author Hiego
  */
 @Service
-public class ServiceProntuario implements IServiceProntuario{
+public class ServiceProntuario implements IServiceProntuario {
+
     @Autowired
     private IDaoProntuario dao;
-    
+
     // salva o prontuario no banco de dados
     @Override
     public void saveProntuario(Prontuario prontuario) {
-       prontuario.setAtivo(true); //seta todo prontuario como ativo por default
-       dao.save(prontuario);
+        try {
+            if (prontuario != null) {
+                prontuario.setAtivo(true); // seta todo prontuario como ativo por default
+                dao.save(prontuario);
+            } else {
+                throw new BusinessException("Prontuário fornecido é nulo.");
+            }
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao salvar o prontuário.", e);
+        }
     }
-    
-    //prontuario ativo
+
+// prontuario ativo
     @Override
     public List<Prontuario> getAllProntuarios() {
-        return this.dao.findAllByAtivoIsTrue();
+        try {
+            List<Prontuario> prontuarios = this.dao.findAllByAtivoIsTrue();
+            if (prontuarios == null) {
+                throw new BusinessException("A lista de prontuários ativos é nula.");
+            }
+            return prontuarios;
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao obter prontuários ativos.", e);
+        }
     }
 
-    //prontuario não ativo
+// prontuario não ativo
     @Override
     public List<Prontuario> getAllArquivados() {
-        return this.dao.findAllByAtivoIsFalse();
+        try {
+            List<Prontuario> arquivados = this.dao.findAllByAtivoIsFalse();
+            if (arquivados == null) {
+                throw new BusinessException("A lista de prontuários arquivados é nula.");
+            }
+            return arquivados;
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao obter prontuários arquivados.", e);
+        }
     }
 
-    //pega o ID
+// pega o ID
     @Override
     public Prontuario findById(Long id) {
-        return this.dao.findById(id);
+        try {
+            if (id != null) {
+                return this.dao.findById(id);
+            } else {
+                throw new BusinessException("O ID fornecido é nulo.");
+            }
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao obter prontuário por ID.", e);
+        }
     }
 
-    //desarquiva o prontuario
+// desarquiva o prontuario
     @Override
     public void desarquivarProntuario(Prontuario prontuario) {
-        prontuario.setAtivo(true);
-        this.dao.save(prontuario);
+        try {
+            if (prontuario != null) {
+                prontuario.setAtivo(true);
+                this.dao.save(prontuario);
+            } else {
+                throw new BusinessException("Prontuário fornecido é nulo.");
+            }
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao desarquivar prontuário.", e);
+        }
     }
 
-    //arquiva o prontuario
+// arquiva o prontuario
     @Override
     public void arquivarProntuario(Prontuario prontuario) {
-       prontuario.setAtivo(false);
-       this.dao.save(prontuario);
-
+        try {
+            if (prontuario != null) {
+                prontuario.setAtivo(false);
+                this.dao.save(prontuario);
+            } else {
+                throw new BusinessException("Prontuário fornecido é nulo.");
+            }
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao arquivar prontuário.", e);
+        }
     }
+
 }
