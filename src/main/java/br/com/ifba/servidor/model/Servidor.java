1558a3c3bd78e2498ca79bd0eae3903b34956304
Davi Andrade Cunha <davi.andrade.2023@hotmail.com>
2023-11-24 06:10:58 -0300

    adicionando service e dao para a classe Servidor

--- src/main/java/br/com/ifba/servidor/model/Servidor.java
+++ src/main/java/br/com/ifba/servidor/model/Servidor.java
@@ -24,6 +24,7 @@ import lombok.Data;
 @Data
 public class Servidor extends Pessoa implements Serializable {
     private String siape;
+    private boolean ativo;
     
     @OneToMany(mappedBy = "servidor", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<FuncaoServidor> funcaoServidor;
